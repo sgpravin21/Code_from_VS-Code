536c5219ca5e9eb50031246d781cc283d4056626
sgpravin21 <sgpravin@gmail.com>
2024-03-14 15:15:41 +0530

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+uname = input("enter your name : \n")
+age = int(input("enter your age : \n"))
+area = input("Enter your area : \n")
+pincode = int(input("enter your area pincode : \n"))
+
+print("Data entered is given below:\n")
+print(uname + " , " + str(age) + " , " + area + " , " + str(pincode) )
